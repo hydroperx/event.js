21caba703e3d2108e24c91b88d93dbbb0a6125e8
hydroperx <hydroperfox@gmail.com>
2025-04-27 09:12:01 -0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
-export type TypedEventTarget<EventMap extends object> =
-  { new (): IntermediateEventTarget<EventMap>; };
+export type TypedEventTarget<EventMap extends object> = {
+  new (): IntermediateEventTarget<EventMap>;
+};
 
 // internal helper type
 interface IntermediateEventTarget<EventMap> extends EventTarget {
   addEventListener<K extends keyof EventMap>(
     type: K,
     callback: (
-      event: EventMap[K] extends Event ? EventMap[K] : never
+      event: EventMap[K] extends Event ? EventMap[K] : never,
     ) => EventMap[K] extends Event ? void : never,
-    options?: boolean | AddEventListenerOptions
+    options?: boolean | AddEventListenerOptions,
   ): void;
 
   addEventListener(
     type: string,
     callback: EventListenerOrEventListenerObject | null,
-    options?: EventListenerOptions | boolean
+    options?: EventListenerOptions | boolean,
   ): void;
 
   removeEventListener<K extends keyof EventMap>(
     type: K,
     callback: (
-      event: EventMap[K] extends Event ? EventMap[K] : never
+      event: EventMap[K] extends Event ? EventMap[K] : never,
     ) => EventMap[K] extends Event ? void : never,
-    options?: boolean | EventListenerOptions
+    options?: boolean | EventListenerOptions,
   ): void;
 
   removeEventListener(
     type: string,
     callback: EventListenerOrEventListenerObject | null,
-    options?: EventListenerOptions | boolean
+    options?: EventListenerOptions | boolean,
   ): void;
 }
